00dc3e7bf52301d99f8b45b8aecc9b0ab1675145
Lucas Tjor <tjor75@gmail.com>
2025-08-18 11:49:58 -0300

    Eliminar opcion de timeout sin uso

--- src/config/api-config.js
+++ src/config/api-config.js
@@ -1,3 +1,2 @@
 export const API_BASE_URL   = import.meta.env.VITE_API_BASE_URL      || "http://localhost:3000";
-export const API_TIMEOUT    = import.meta.env.VITE_API_TIMEOUT       || 5000;
 export const API_PAGE_LIMIT = import.meta.env.VITE_API_PAGE_LIMIT    || 6;
